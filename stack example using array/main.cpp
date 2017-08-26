ccb75b36e94ec8d20e4235f41e63577ab2cd8fd8
Ross Spencer <rossspencer@rosss-macbook-pro-2.local>
2017-08-26 15:34:09 -0400

    Initial Commit

---
+++ stack example using array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stack example using array
+//
+//  Created by Ross Spencer on 8/26/17.
+//  Copyright © 2017 Ross Spencer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
